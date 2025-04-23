c38cf4c5623171918cac5dba501a9f4188caa513
Jesus Bejar <israelbejarii@gmail.com>
2025-04-23 10:16:57 -0600

    made github authentication

--- app/auth-button.tsx
+++ app/auth-button.tsx
@@ -1,8 +1,17 @@
 "use client"
 
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
+
 export default function AuthButton() {  
+    const supabase = createClientComponentClient()
     const handleLogin = async () => {
-        console.log("login")
+        await supabase.auth.signInWithOAuth({
+            provider: "github", 
+            options: {
+                redirectTo: "https://http://localhost:3000/auth/callback"
+            }
+        })
+
     }
     return (
         <button onClick={() => {handleLogin()}}>
